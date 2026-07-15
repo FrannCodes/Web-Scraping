dbef8978a51411848c26ec6048fc84d32ce35436
Nnenna Nnebe <franneci32@gmail.com>
2026-07-14 21:36:37 -0400

    Completed Phase 2, will now make a it a class

--- phase2.py
+++ phase2.py
@@ -32,46 +32,57 @@ while not end_of_page:
         product = p.find("a")
         # add product page urls to an array of product_page_url:
         product_page_url.append(product.get("href").replace("../../..", "https://books.toscrape.com/catalogue", 1))
-
     if next_page is None:
         end_of_page = True
     else:
         url_section = next_page.find("a").get("href")
         url = "https://books.toscrape.com/catalogue/category/books/fantasy_19/" + url_section
-
+################################################################################################
 print(product_page_url)
 # find product information per product url
 for url_product in product_page_url:
     product_page = requests.get(url_product)
     product_doc = BeautifulSoup(product_page.content, "html.parser")
-    product_information_doc = product_doc.find("table", class_="table table-striped")
 
+    # UPC, Quantity Available, and Prices section:
+    product_information_doc = product_doc.find("table", class_="table table-striped")
+    #######################################################################################
     # Universal Product Code (UPC)
     universal_product_code.append(product_information_doc.find_all("td")[0].string)
     # Price Excluding Tax
     price_excluding_tax.append(product_information_doc.find_all("td")[2].string)
     # Price Including Tax
     price_including_tax.append(product_information_doc.find_all("td")[3].string)
+    # Quantity Available
+    quantity_available.append(product_information_doc.find_all("td")[5].string)
+
+    # Book Title and Star rating section:
+    product_information_doc1 = product_doc.find("div", class_ = "col-sm-6 product_main")
+    ##########################################################################################
+    # Book Title
+    book_title.append(product_information_doc1.find("h1").string)
+    # Review Rating
+    review_rating.append(product_information_doc1.find("p", class_ = "star-rating").get("class")[1])
+
+    #Product Description section:
+    product_information_doc2 = product_doc.find("article", class_ = "product_page")
+    #####################################################################################
+    # Product Description
+    product_description.append(product_information_doc2.find("p", class_ = None).string)
+
+    #Category Section:
+    product_information_doc3 = product_doc.find("ul", class_ = "breadcrumb")
+    ##################################################################
+    # Category
+    category.append(product_information_doc3.find("a", href = "../category/books/fantasy_19/index.html").string)
+
+    # Image Section:
+    product_information_doc4 = product_doc.find("img")
+    ##################################################
+    # Image
+    image_url.append(product_information_doc4.get("src").replace("../..", "https://books.toscrape.com", 1))
 
 
-print(universal_product_code)
-
-
-# Universal product code
-
-#Book Title
-
-#Price Including Tax
-
-#Price Excluding Tax
-
-#Quantity Available
-
-#Product Description
-
-#Category
-
-#Review Rating
 
 #Image URL
 
