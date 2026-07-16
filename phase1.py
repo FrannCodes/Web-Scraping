a0c50affe5e9e08966db991bc4fc2905d4dd8a66
Nnenna Nnebe <franneci32@gmail.com>
2026-07-16 19:55:57 -0400

    Phase3 and Phase1 Class completed

--- phase1.py
+++ phase1.py
@@ -7,64 +7,76 @@ url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html
 class Scraper:
     def __init__(self, url):
         self.url = url
+        self.book_title = ""
+        self.product_page_url = ""
+        self.universal_product_code = ""
+        self.price_including_tax = ""
+        self.price_excluding_tax = ""
+        self.quantity_available = ""
+        self.product_description = ""
+        self.category = ""
+        self.review_rating = ""
+        self.image_url = ""
 
-page = requests.get(url)
-doc = BeautifulSoup(page.content, "html.parser")
+    def scrape(self):
+        page = requests.get(url)
+        doc = BeautifulSoup(page.content, "html.parser")
 
-#Product Page URL
-product_page_url = page.url
+        # Product Page URL
+        self.product_page_url = page.url
 
-# Universal product code
-universal_product_code_doc = doc.find_all("tr")[0]
-universal_product_code = universal_product_code_doc.find("td").string
-print(universal_product_code)
+        # Universal product code
+        universal_product_code_doc = doc.find_all("tr")[0]
+        self.universal_product_code = universal_product_code_doc.find("td").string
 
-#Book Title
-book_title = doc.title.string
-print(book_title)
+        # Book Title
+        self.book_title = doc.title.string.strip()
 
-#Price Including Tax
-price_including_tax_doc = doc.find_all("tr")[3]
-price_including_tax = price_including_tax_doc.find("td").string
-print(price_including_tax)
+        # Price Including Tax
+        price_including_tax_doc = doc.find_all("tr")[3]
+        self.price_including_tax = price_including_tax_doc.find("td").string
 
-#Price Excluding Tax
-price_excluding_tax_doc = doc.find_all("tr")[2]
-price_excluding_tax = price_excluding_tax_doc.find("td").string
-print(price_excluding_tax)
+        # Price Excluding Tax
+        price_excluding_tax_doc = doc.find_all("tr")[2]
+        self.price_excluding_tax = price_excluding_tax_doc.find("td").string
 
-#Quantity Available
-quantity_available_doc = doc.find_all("tr")[5]
-quantity_available = quantity_available_doc.find("td").string
-print(quantity_available)
+        # Quantity Available
+        quantity_available_doc = doc.find_all("tr")[5]
+        self.quantity_available = quantity_available_doc.find("td").string
 
-#Product Description
-product_description_doc = doc.find("article", class_="product_page")
-product_description = product_description_doc.find_all("p")[-1].string
-print(product_description)
+        # Product Description
+        product_description_doc = doc.find("article", class_="product_page")
+        self.product_description = product_description_doc.find_all("p")[-1].string
 
-#Category
-category_doc = doc.find_all("li")[2]
-category = category_doc.find("a").string
-print(category)
+        # Category
+        category_doc = doc.find_all("li")[2]
+        self.category = category_doc.find("a").string
 
-#Review Rating
-review_rating_doc = doc.find("p", class_ = "star-rating")
-review_rating = review_rating_doc.get("class")[1]
-print(f"{review_rating} out of Five Stars")
+        # Review Rating
+        review_rating_doc = doc.find("p", class_="star-rating")
+        self.review_rating = review_rating_doc.get("class")[1]
 
-#Image URL
-image_url_doc = doc.find("img")
-image_url_text = image_url_doc.get("src")
-image_url = image_url_text.replace("../..", "https://books.toscrape.com", 1)
-print(image_url)
+        # Image URL
+        image_url_doc = doc.find("img")
+        image_url_text = image_url_doc.get("src")
+        self.image_url = image_url_text.replace("../..", "https://books.toscrape.com", 1)
 
-#CSV file
-headers = ["Book Title", "Product Page URL", "Universal Product Code (UPC)", "Price Including Tax", "Price Excluding Tax", "Quantity Available", "Product Description", "Category", "Review Rating", "Image URL"]
-columns = [book_title, product_page_url, universal_product_code, price_including_tax, price_excluding_tax, quantity_available, product_description, category, review_rating, image_url]
-with open("phase1.csv", "w", newline = "") as file:
-    #make a writer object using the file
-    writer = csv.writer(file, delimiter = ",")
-    #create headers and columns
-    writer.writerow(headers)
-    writer.writerow(columns)
+    def csv_file(self):
+        # CSV file
+        headers = ["Book Title", "Product Page URL", "Universal Product Code (UPC)", "Price Including Tax",
+                   "Price Excluding Tax", "Quantity Available", "Product Description", "Category", "Review Rating",
+                   "Image URL"]
+        columns = [self.book_title, self.product_page_url, self.universal_product_code, self.price_including_tax, self.price_excluding_tax,
+                   self.quantity_available, self.product_description, self.category, self.review_rating, self.image_url]
+        with open("phase1.csv", "w", newline="") as file:
+            # make a writer object using the file
+            writer = csv.writer(file, delimiter=",")
+            # create headers and columns
+            writer.writerow(headers)
+            writer.writerow(columns)
+
+scraper = Scraper(url)
+scraper.scrape()
+scraper.csv_file()
+
+print(scraper.book_title)
